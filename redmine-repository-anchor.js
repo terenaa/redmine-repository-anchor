6cadea95e2eb508e6389054e2bc9478a8ad679bc
Krzysztof Janda <terenaa@the-world.pl>
2016-01-22 19:18:07 +0100

    Create redmine-repository-anchor.js

---
+++ redmine-repository-anchor.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Redmine repository hyperlink
+// @namespace    http://www.the-world.pl/
+// @version      0.1
+// @author       Krzysztof Janda <terenaa@the-world.pl>
+// @match        http://redmine.krakvet.pl/issues/*
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+var isRepositoryActive = (document.getElementsByClassName('repository').length == 1);
+
+if (isRepositoryActive) {
+    var taskNumberElement = document.getElementsByTagName('h2')[0],
+        projectName = document.getElementsByTagName('h1')[0].innerText.toLowerCase(),
+        categoryName = document.getElementsByClassName('category')[1].innerText,
+        taskNumberElementParts = taskNumberElement.innerText.split('#'),
+        anchor = document.createElement('a');
+    
+    anchor.title = 'Przejdź do repozytorium';
+    anchor.href = '/projects/' + projectName + '/repository/show/' + categoryName + '/branches/' + taskNumberElementParts[1];
+    anchor.innerText = '#' + taskNumberElementParts[1];
+    
+    taskNumberElement.innerText = taskNumberElementParts[0] + ' ';
+    taskNumberElement.appendChild(anchor);
+}
